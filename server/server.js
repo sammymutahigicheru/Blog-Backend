a823f240705bffeb638ad9291a8142bb1ae4a526
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-05 10:41:10 +0300

    register observers

--- server/server.js
+++ server/server.js
@@ -32,3 +32,19 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+app.models.User.afterRemote('create', (ctx,user,next) =>{
+  console.log('New User is:',user);
+  app.models.Profile.create({
+    first_name: user.username,
+    created_at: new Date(),
+    userId: user.id
+  }, (err,result) =>{
+    if(!err && result){
+      console.log("Created new profile!",result);
+    }else{
+      console.log("There is an error",err);
+    }
+  });
+  next();
+});
