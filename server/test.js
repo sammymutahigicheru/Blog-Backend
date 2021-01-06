8412565ea480fc596715b72874471b103bd4dc1b
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-06 09:20:47 +0300

    update attributes in mongo

--- server/test.js
+++ server/test.js
@@ -1,5 +1,13 @@
 var models = require('./server.js').models;
 
 models.Profile.findOrCreate({name:'sammy'},(err,profile) =>{
-    console.log("data?",err,profile);
+    if (err){
+        console.log("There was an error",err);
+    }else if(profile){
+        profile.updateAttribute({
+            email:"sammymutahi21@gmail.com"
+        },(updateError,updated) =>{
+            console.log("saved?",updateError, updated);
+        })
+    }
 })
