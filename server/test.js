f98230b4b77429b7811b0c5deb0bbf340d3b7fae
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-06 09:33:07 +0300

    create a bunch of random data and save  to db

--- server/test.js
+++ server/test.js
@@ -1,13 +1,21 @@
 var models = require('./server.js').models;
 
-models.Profile.findOrCreate({name:'sammy'},(err,profile) =>{
-    if (err){
-        console.log("There was an error",err);
-    }else if(profile){
-        profile.updateAttributes({
-            email:"sammymutahi12@gmail.com"
-        },(updateError,updated) =>{
-            console.log("saved?",updateError, updated);
-        })
-    }
+var toSave = [
+    {name:'sammy',email:"sammymutahi12@gmail.com"},
+    {name:'sammy1',email:"sammymutahi121@gmail.com"},
+    {name:'sammy2',email:"sammymutahi122@gmail.com"},
+    {name:'sammy3',email:"sammymutahi123@gmail.com"},
+    {name:'sammy4',email:"sammymutahi123@gmail.com"},
+    {name:'sammy5',email:"sammymutahi125@gmail.com"},
+    {name:'sammy6',email:"sammymutahi126@gmail.com"},
+    {name:'sammy7',email:"sammymutahi127@gmail.com"},
+    {name:'sammy8',email:"sammymutahi128@gmail.com"},
+    {name:'sammy9',email:"sammymutahi129@gmail.com"},
+    {name:'sammy0',email:"sammymutahi120@gmail.com"},
+];
+
+toSave.map(obj =>{
+    models.Profile.create(obj, (err,result) =>{
+        console.log("Created?",result);
+    })
 })
