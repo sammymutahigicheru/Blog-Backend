82077eca5441e4350bae27d2e4c718d8d335f5a5
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-06 09:27:44 +0300

    update test.js

--- server/test.js
+++ server/test.js
@@ -4,8 +4,8 @@ models.Profile.findOrCreate({name:'sammy'},(err,profile) =>{
     if (err){
         console.log("There was an error",err);
     }else if(profile){
-        profile.updateAttribute({
-            email:"sammymutahi21@gmail.com"
+        profile.updateAttributes({
+            email:"sammymutahi12@gmail.com"
         },(updateError,updated) =>{
             console.log("saved?",updateError, updated);
         })
